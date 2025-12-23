bf0939c6d9f25059d1e125c830149f26704642b2
Nehuen Lian <nehuenn.lian@gmail.com>
2025-12-23 18:40:46 -0300

    verify response status after requesting access token (TA)

--- service/controllers/request_access_token_controller.py
+++ service/controllers/request_access_token_controller.py
@@ -1,5 +1,5 @@
 from service.crypto.sign import get_binary_cms, sign_login_ticket_request
-from service.soap_management.soap_client import login_cms
+from service.soap_client.wsaa import login_cms
 from service.utils.logger import logger
 from service.xml_management.xml_builder import (
     build_login_ticket_request, parse_and_save_loginticketresponse, save_xml)
@@ -12,13 +12,17 @@ def generate_token_from_scratch() -> None:
     sign_login_ticket_request()
     b64_cms = get_binary_cms()
     login_ticket_response = login_cms(b64_cms)
-    parse_and_save_loginticketresponse(login_ticket_response)
-    logger.info("Token generated.")
+
+    if login_ticket_response["status"] == "success":
+        parse_and_save_loginticketresponse(login_ticket_response["response"])
+        logger.info("Token generated.")
 
 def generate_token_from_existing() -> None:
 
     logger.info("Token still valid. Generating loginTicketResponse without signing or creating new loginTicketRequest.")
     b64_cms = get_binary_cms()
     login_ticket_response = login_cms(b64_cms)
-    parse_and_save_loginticketresponse(login_ticket_response)
-    logger.info("Token generated.")
+
+    if login_ticket_response["status"] == "success":
+        parse_and_save_loginticketresponse(login_ticket_response["response"])
+        logger.info("Token generated.")
